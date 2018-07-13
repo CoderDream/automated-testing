9b1be3d06303cb9950fa1b2580c78573dfd15fca
CoderDream <coderdream@gmail.com>
2018-07-13 09:20:47 +0800

    更新.gitignore文件

--- src/test/java/com/billjc/pdrc/service/LoginServiceTest.java
+++ src/test/java/com/billjc/pdrc/service/LoginServiceTest.java
@@ -53,6 +53,5 @@ public class LoginServiceTest {
 		Runtime.getRuntime().exec("taskkill /F /im " + "chromedriver.exe");
 		// Runtime.getRuntime().exec("taskkill /F /im " + "chrome.exe");
 		// 结束进程。以上是用于在发生异常时结束chromedriver进程，避免chromedriver占用内存资源。close()，和quit()方法有时候是无法结束chromedriver进程的。
-
 	}
 }
